9422886f0ede0120dfefb281caae50c3ecdc70dc
Brian Hurst <65836004+brianhurst314@users.noreply.github.com>
2021-01-30 12:50:31 -0600

    Add files via upload
    C++ Timer class

---
+++ Timer.h
@@ -0,0 +1,46 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#include<chrono>
+#include<iostream>
+
+using namespace std;
+
+//typedef to make less typing
+typedef chrono::high_resolution_clock myClock;
+
+class Timer
+{
+	//time point variable to hold the starting time
+	chrono::time_point<myClock> begin;
+
+	//duration variable to hold the total seconds passed
+	//between start() and stop() stored as a double
+	chrono::duration<double> elapsedTime;
+
+public:
+
+	void start()
+	{
+		//assign the current time to begin
+		begin = myClock::now();
+	}
+
+	void stop()
+	{
+		//take the current time and subtract the begin time
+		//and store the results in elapsedTime
+		elapsedTime = myClock::now() - begin;
+	}
+
+	//overload the ostream operator for outputting the elapsed time
+	friend ostream& operator << (ostream&, const Timer&);
+};
+
+ostream& operator << (ostream& out, const Timer& t)
+{
+	out << t.elapsedTime.count();
+	return out;
+}
+
+#endif /* TIMER_H_ */
